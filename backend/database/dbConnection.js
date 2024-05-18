3b08cb938f149d2e14e3dbc06a719b3097f35fc8
anshul jangale <anshuljangale000@gmail.com>
2024-05-18 09:05:20 +0530

    commit

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/jobseek", {
+      dbName: "MERN_JOB_SEEKING_WEBAPP",
+    })
+    .then(() => {
+      console.log("Connected to database.");
+    })
+    .catch((err) => {
+      console.log(`Some Error occured. ${err}`);
+    });
+};
